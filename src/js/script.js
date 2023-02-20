1cca7541985519a743682fe781459dfc9c345685
Eleonora <sergeeva.nora@inbox.ru>
2023-02-20 18:32:18 +0300

    слайдер без автопрокрутки и touch

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,34 @@
-// весь код взяли на сайте slick
+//Устанавливаем стартовый индекс слайда по умолчанию:
+let slideIndex = 1;
+//Вызываем функцию, которая реализована ниже:
+showSlides(slideIndex);
 
-$(document).ready(function () { // обратились ко всему документу html, когда он будет полностью готов
-	$('.carousel__wrapper').slick({ // все настройки для слайдера можно найти на сайте slick
-		speed: 1200, // скорость прокрцтки слайдов
-		adaptiveHeight: true, // высота слайда будет подстраиваться под высоту картинки в нем
-		prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"</button>',
-		nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"</button>',
-		responsive: [ 
-			{
-				breakpoint: 768,
-				settings: {
-					arrows: false,
-					dots: true
-				}
-			}
-		]
-	});
-});
+// Вперед/назад элементы управления (Увеличиваем индекс на n — показываем следующий слайд)
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+
+// Устанавливаем текущий слайд (эемент управления - кружочек под слайдом)
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+//функция перелистывания
+function showSlides(n) {
+	let i;
+	//Обращаемся к элементам с названием класса "carousel__slide", то есть к картинкам:
+	let slides = document.getElementsByClassName("carousel__slide");
+	let dots = document.getElementsByClassName("dot");
+	//Проверяем количество слайдов:
+	if (n > slides.length) { slideIndex = 1 }
+	if (n < 1) { slideIndex = slides.length }
+	//Проходим по каждому слайду в цикле for:
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	//Делаем элемент блочным:
+	slides[slideIndex - 1].style.display = "block";
+	dots[slideIndex - 1].className += " active";
+}
