0e98d09a77dd63e321b6f05cbea69e0e5669e470
Eleonora <sergeeva.nora@inbox.ru>
2023-02-21 16:45:25 +0300

    norm slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,19 @@
-//Устанавливаем стартовый индекс слайда по умолчанию:
-let slideIndex = 1;
-//Вызываем функцию, которая реализована ниже:
-showSlides(slideIndex);
+$(document).ready(function () {
+	$('.carousel__wrapper').slick({
+		speed: 1200,
+		adaptiveHeight: true,
+		prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+		responsive: [
+			{
+				breakpoint: 768, // 767px и ниже
+				settings: {
+					dotsClass: 'slick-dots',
+					dots: true,
+					arrows: false
+				}
+			}
+		]
+	});
 
-// Вперед/назад элементы управления (Увеличиваем индекс на n — показываем следующий слайд)
-function plusSlides(n) {
-	showSlides(slideIndex += n);
-}
-
-// Устанавливаем текущий слайд (эемент управления - кружочек под слайдом)
-function currentSlide(n) {
-	showSlides(slideIndex = n);
-}
-//функция перелистывания
-function showSlides(n) {
-	let i;
-	//Обращаемся к элементам с названием класса "carousel__slide", то есть к картинкам:
-	let slides = document.getElementsByClassName("carousel__slide");
-	let dots = document.getElementsByClassName("dot");
-	//Проверяем количество слайдов:
-	if (n > slides.length) { slideIndex = 1 }
-	if (n < 1) { slideIndex = slides.length }
-	//Проходим по каждому слайду в цикле for:
-	for (i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none";
-	}
-	for (i = 0; i < dots.length; i++) {
-		dots[i].className = dots[i].className.replace(" active", "");
-	}
-	//Делаем элемент блочным:
-	slides[slideIndex - 1].style.display = "block";
-	dots[slideIndex - 1].className += " active";
-}
+});
