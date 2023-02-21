f90ba467e941b37cc8eedacd877da7ca9846da28
Eleonora <sergeeva.nora@inbox.ru>
2023-02-21 18:15:42 +0300

    super slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,17 @@
-$(document).ready(function () {
-	$('.carousel__wrapper').slick({
-		speed: 1200,
-		adaptiveHeight: true,
-		prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-		nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-		responsive: [
-			{
-				breakpoint: 768, // 767px и ниже
-				settings: {
-					dotsClass: 'slick-dots',
-					dots: true,
-					arrows: false
-				}
-			}
-		]
-	});
+const slider = tns({
+	container: '.carousel__wrapper',
+	items: 1,
+	slideBy: 'page',
+	autoplay: false,
+	controls: false, // убрали кастомные стрелочки
+	navPosition: "bottom" // разместили кружочки внизу слайдера
+});
 
+
+document.querySelector('.prev').addEventListener('click', function () { // назначаем созданные вручную кнопки для перелистывания слайдов
+	slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () { // назначаем созданные вручную кнопки для перелистывания слайдов
+	slider.goTo('next');
 });
